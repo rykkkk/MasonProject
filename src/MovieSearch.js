b55dd07785cf43c3362dcf6632414bc191becd5f
ryk-k <rykk@sheridancollege.ca>
2024-04-24 11:27:14 -0400

    ontario design system

---
+++ src/MovieSearch.js
@@ -0,0 +1,28 @@
+
+
+export const MovieSearch = ({
+    type,
+    className,
+    name,
+    placeholder,
+    onChange = () => { },
+    onBlur = () => { },
+    onFocus = () => { },
+    value,
+}) => {
+    return (
+        <>
+            <input
+                type={type}
+                className={className}
+                name={name}
+                placeholder={placeholder}
+                required
+                onChange={onChange}
+                onBlur={onBlur}
+                onFocus={onFocus}
+                value={value}
+            />
+        </>
+    );
+};
