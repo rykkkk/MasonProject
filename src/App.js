771409e740a74c46f3da6ab644868034ed631d29
krista <krista.ryk@ontario.ca>
2024-04-25 12:00:58 -0400

    everything

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 import React from "react";
 import { OPSFooter, OPSHeader } from "./components/Layout";
-import {  Output ,MovieResults} from "./MovieSearch";
+import { Output, MovieResults } from "./MovieSearch";
 
 function App() {
 	return (
 		<div>
 			<OPSHeader></OPSHeader>
-
 			<Output></Output>
-<MovieResults></MovieResults>		<OPSFooter></OPSFooter>
+			<MovieResults></MovieResults> 
+            <OPSFooter></OPSFooter>
 		</div>
 	);
 }
